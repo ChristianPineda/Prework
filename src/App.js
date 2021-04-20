59c1d5d735c7d632e0102f315b19a0eba798d894
pincode <viajero06701@innovaccion.mx>
2021-04-19 22:57:36 -0500

    Símbolo React

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React
+          Aprende React!
         </a>
       </header>
     </div>
